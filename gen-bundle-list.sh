4cb2c31446aff1f248b8b3e82c6e28011d126dd9
Jamie McConnell <jamie_mcconnell@icloud.com>
2017-02-15 21:36:00 +0000

    more plugins

--- gen-bundle-list.sh
+++ gen-bundle-list.sh
@@ -2,4 +2,4 @@
 # Thanks Jess https://github.com/jessfraz/.vim
 set -e
 
-git  submodule --quiet foreach bash -c "echo -e \"* [\$(git config --get remote.origin.url | sed 's#https://##' | sed 's#git://##' | sed 's/.git//')](\$(git config --get remote.origin.url))\""
+git submodule --quiet foreach bash -c "echo -e \"* [\$(git config --get remote.origin.url | sed 's#https://##' | sed 's#git://##' | sed 's/.git//')](\$(git config --get remote.origin.url))\""
